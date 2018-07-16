85f8096b319cd7fd204f3d7fb22fd9b590c8755c
kaustavchatterjee <kaustav.chatterjee@nearbuy.com>
2018-07-16 15:39:38 +0530

    Initial commit

---
+++ src/main/java/com/mytest/springboot/kaustavservice/Test.java
@@ -0,0 +1,8 @@
+package com.mytest.springboot.kaustavservice;
+
+public class Test {
+
+    public String id;
+    public String name;
+
+}
---
+++ src/main/java/com/mytest/springboot/kaustavservice/TestController.java
@@ -0,0 +1,19 @@
+package com.mytest.springboot.kaustavservice;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @ResponseBody
+    @RequestMapping("/test")
+    public Test getTest(){
+        Test test = new Test();
+        test.id = "12";
+        test.name = "Kaustav";
+        return test;
+    }
+}
